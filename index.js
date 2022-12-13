d2fe87eb58bac1e277f1e73c9b117179f49ca2ba
Allam Nikhil Sai <2019198@iiitdmj.ac.in>
2022-12-13 21:57:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors')
+const path = require('path');
+require('dotenv').config({path: __dirname + '/.env'})
+const app = express();
+app.use(cors({
+  origin: "*"
+}))
+app.use(express.json());
+var router = require('./mainrouter');
+
+app.listen(5000, () => {
+    console.log('Server Started')
+});
+
+mongoose.connect(process.env.Mongo_Url,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }).then(()=>{
+    console.log('Connected to mongo server');
+  }).catch(err=>{
+    console.log(err);
+});
+app.use(router);
